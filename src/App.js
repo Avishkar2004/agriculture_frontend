c105454ef55554088c21c8726565687cd7b6d345
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-10-20 21:22:05 +0530

    Next Product Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import AppRoutes from "./Routes/Routes";
 import { AuthProvider } from "./actions/authContext";
+
 function App() {
   return (
-    <div>
-      <AuthProvider>
-        <AppRoutes />
-      </AuthProvider>
-    </div>
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
   );
 }
 
